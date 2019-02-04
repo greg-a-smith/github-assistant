ae642d3ce2d8e963b2ad77fc5849e26647d61600
Greg Smith <greg.smith03@sap.com>
2019-02-04 16:13:33 -0600

    chore: Added debug statements

--- ci-scripts/publish-rc.sh
+++ ci-scripts/publish-rc.sh
@@ -5,10 +5,15 @@ set -e
 git config --global user.email "greg.smith03@sap.com"
 git config --global user.name "Greg Smith"
 
+git status
+git remote -v
+
 # update the package verion and commit to the git repository
 npm run std-version -- --prerelease rc --no-verify
 
+git status
+
 # pushes changes to master
-git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "$TRAVIS_BRANCH"
+git push --verbose --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "$TRAVIS_BRANCH"
 
 npm publish --tag prerelease
