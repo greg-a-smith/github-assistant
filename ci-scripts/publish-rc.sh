70f44890285a8f308214c73f6dd1f5e29dab8ce2
Greg Smith <greg.smith03@sap.com>
2019-02-04 23:26:38 -0600

    chore: Updated scripts, updated babel config and implemented pre-commit

--- ci-scripts/publish-rc.sh
+++ ci-scripts/publish-rc.sh
@@ -5,15 +5,8 @@ set -e
 git config --global user.email "greg.smith03@sap.com"
 git config --global user.name "Greg Smith"
 
-git status
-git remote -v
-
-# update the package verion and commit to the git repository
 npm run std-version -- --prerelease rc --no-verify
 
-git status
-
-# pushes changes to master
-git push --verbose --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "$TRAVIS_BRANCH"
+git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" "$TRAVIS_BRANCH"
 
 npm publish --tag prerelease
--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -5,19 +5,16 @@ set -e
 git config --global user.email "greg.smith03@sap.com"
 git config --global user.name "Greg Smith"
 
-git checkout master
-npm install
-
-# delete tmp_branch_for_automated_release_do_not_use branch
-git push "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" :do_not_use_release_branch
+# delete temp branch
+git push "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" ":$TRAVIS_BRANCH"
 
 std_ver=$(npm run std-version)
 release_tag=$(echo "$std_ver" | grep "tagging release" | awk '{print $4}')
 
 echo "$std_ver"
 
-git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" master > /dev/null 2>&1;
+git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" master
 
-npm run release:create -- --tag $release_tag --debug
+npm run release:create -- --tag $release_tag
 
 npm publish
