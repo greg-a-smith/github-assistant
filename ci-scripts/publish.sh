93b465e69a3cc9f30bcfb7940efc7fc8b4ceea7b
Greg Smith <greg.smith03@sap.com>
2019-02-05 10:06:08 -0600

    fix: Added appropriate arguments for call to create-release (#1)

--- ci-scripts/publish.sh
+++ ci-scripts/publish.sh
@@ -15,6 +15,6 @@ echo "$std_ver"
 
 git push --follow-tags "https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG" master
 
-npm run release:create -- --tag $release_tag
+npm run release:create -- --repo $TRAVIS_REPO_SLUG --tag $release_tag --branch master
 
 npm publish
